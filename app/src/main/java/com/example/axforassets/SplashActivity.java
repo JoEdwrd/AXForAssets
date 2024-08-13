ddaba2b7f655a3fc1eaf33b6b1a08c4e945447fa
“dustin-arasy” <dustin.bijou@gmail.com>
2024-08-13 23:33:07 +0700

    fixing some bugs

--- app/src/main/java/com/example/axforassets/SplashActivity.java
+++ app/src/main/java/com/example/axforassets/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, Profile.class);
+                Intent intent = new Intent(SplashActivity.this, LoginPageActivity.class);
                 startActivity(intent);
                 finish();
             }
